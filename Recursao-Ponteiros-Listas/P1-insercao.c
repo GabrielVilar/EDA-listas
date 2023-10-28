384835b60e22ce07b45d3a5383bc63f1fdfe8731
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 09:12:30 -0300

    P1 inserção

---
+++ Recursao-Ponteiros-Listas/P1-insercao.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}celula;
+
+void insere_inicio(celula *le, int x);
+void insere_antes(celula *le, int x, int y);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+void insere_inicio(celula *le, int x){
+
+}
+
+void insere_antes(celula *le, int x, int y){
+    
+}
