a04668e5c39bdd2c50f6c24ffd1be65b98d9dc76
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-27 21:22:13 -0300

    new

--- Listao_aquece/Extra-TimeTravel.c
+++ Listao_aquece/Extra-TimeTravel.c
@@ -1,39 +1,3 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// int timeltravel(int Gates, int GateIwant, int whereIam, int Past, int Future);
-// int main(int argc, char const *argv[])
-// {
-//     printf("%d\n", timeltravel(100,12,2,1,2));
-    
-//     return 0;
-// }
-// int timeltravel(int Gates, int GateIwant, int whereIam, int Past, int Future){
-//     int gatesUp, gatesDown, cont1 = 0, cont2 = 0;
-//     gatesUp = Gates - whereIam;
-//     gatesDown = (Gates - (gatesUp + 1));
-
-//     if (GateIwant < whereIam){
-//         for (int i = gatesDown; i >= 0; i = i - Past){
-//             cont1++;
-//             if (i == GateIwant)
-//             {
-//                 return cont1;
-//             }else{
-//                 return -1;
-//             }
-//         }
-//     }else{
-//         for (int i = whereIam; i <= gatesUp; i= i + Future){
-//             cont2++;
-//             if (i == GateIwant)
-//             {
-//                 return cont2;
-//             }else{
-//                 return -1;
-//             }
-//         }
-//     }
-// }
 #include <stdio.h>
 #include <stdlib.h>
 
