95ae6c2a301d23f809e8397b6af276c3dc0b0b10
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 09:07:35 -0300

    dividir

---
+++ Recursao-Ponteiros-Listas/N1-Dividir.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}celula;
+
+void divide_lista(celula *1, celula *11, celula *12);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+void divide_lista(celula *1, celula *11, celula *12){
+    
+}
