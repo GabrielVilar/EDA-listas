9e15c3cb16489c8e8160a41deae4cd378dc8725b
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 09:03:52 -0300

    M1 new change

--- Recursao-Ponteiros-Listas/M1-lista-buscar.c
+++ Recursao-Ponteiros-Listas/M1-lista-buscar.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}celula;
+
+celula *busca (celula *le, int x);
+celula *busca_recursiva (celula *le, int x);
+
 int main(){
     
     return 0;
+}
+
+celula *busca (celula *le, int x){
+
+}
+
+celula *busca_recursiva (celula *le, int x){
+    
 }
