379e6f338adf0c0ea6a192aa40b351326e7849fb
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 09:18:53 -0300

    R1 remoção

---
+++ Recursao-Ponteiros-Listas/R1-remocao.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}celula;
+
+int remove_depois(celula *p);
+void remove_elemento(celula *le, int x);
+void remove_todos_elementos(celula *le, int x);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+int remove_depois(celula *p){
+
+}
+
+void remove_elemento(celula *le, int x){
+
+}
+
+void remove_todos_elementos(celula *le, int x){
+    
+}
