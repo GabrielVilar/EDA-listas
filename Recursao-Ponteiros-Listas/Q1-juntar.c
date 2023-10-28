ba990c2056d502c69c8cb6038a782a318473dc67
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 09:15:49 -0300

    Q1 juntar

---
+++ Recursao-Ponteiros-Listas/Q1-juntar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}celula;
+
+void mescla_lista(celula *11, celula *12, celula *13);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+void mescla_lista(celula *11, celula *12, celula *13){
+    
+}
