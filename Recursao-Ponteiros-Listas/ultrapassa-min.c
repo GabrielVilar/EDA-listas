a8b29fdf3597b01eb9bc3b3b799779f55d7a6c46
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 10:05:38 -0300

    new changes

--- Recursao-Ponteiros-Listas/ultrapassa-min.c
+++ Recursao-Ponteiros-Listas/ultrapassa-min.c
@@ -1,52 +1,125 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(int argc, char const *argv[])
+// {
+//     int *vetor; 
+//     int numero, barra, tamanho = 0, capacidade = 0;
+//     vetor = (int *)malloc(tamanho * sizeof(int));
+//     int *vetorAux;
+//     int tamanhoAux = 0, capacidadeAux = 0;
+//     vetorAux = (int *)malloc(tamanhoAux * sizeof(int));
+//     int soma = 0, cont = 0, ultimoNumero = 0, ultimoIndice = -1;
+//     do
+//     {
+//         scanf("%d", &numero);
+       
+//         if(tamanho == capacidade){
+//             capacidade += 1; 
+//             vetor = (int *) realloc(vetor, capacidade * sizeof(int)); 
+//         }
+        
+//         if (numero != 0){
+//             vetor[tamanho] = numero;
+//             tamanho++;
+//         }
+
+//     } while (numero != 0);
+
+//     scanf("%d", &barra);
+
+//     for (int i = 0; i < tamanho; i++){
+       
+//         soma += vetor[i];
+//         if (soma >= barra){
+//             cont++;
+//             soma = 0;
+//             if(tamanhoAux == capacidadeAux){
+//                 capacidadeAux += 1;
+//                 vetorAux = (int *) realloc(vetorAux, capacidadeAux * sizeof(int));
+//             }
+//             vetorAux[tamanhoAux] = i;
+//             tamanhoAux++;
+//         }
+//     }
+    
+//     for (int i = tamanhoAux - 1; i >= 0; i--){
+//         printf("%d\n",vetor[vetorAux[i]]);
+//     }
+   
+//     free(vetor); 
+//     free(vetorAux); 
+//     return 0;
+// }
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char const *argv[])
-{
-    int *vetor; 
-    int numero, barra, tamanho = 0, capacidade = 0;
-    vetor = (int *)malloc(tamanho * sizeof(int));
-    int *vetorAux;
-    int tamanhoAux = 0, capacidadeAux = 0;
-    vetorAux = (int *)malloc(tamanhoAux * sizeof(int));
-    int soma = 0, cont = 0, ultimoNumero = 0, ultimoIndice = -1;
-    do
-    {
-        scanf("%d", &numero);
-       
-        if(tamanho == capacidade){
-            capacidade += 1; 
-            vetor = (int *) realloc(vetor, capacidade * sizeof(int)); 
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+Node *createNode(int value) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->value = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insert(Node **head, int value) {
+    Node *newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        Node *current = *head;
+        while (current->next != NULL) {
+            current = current->next;
         }
-        
-        if (numero != 0){
-            vetor[tamanho] = numero;
-            tamanho++;
-        }
+        current->next = newNode;
+    }
+}
 
-    } while (numero != 0);
+void printAndFree(Node **head) {
+    Node *current = *head;
+    while (current != NULL) {
+        printf("%d\n", current->value);
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+    *head = NULL;
+}
 
-    scanf("%d", &barra);
+int main() {
+    int value;
+    Node *subsequence = NULL;
+    int sum = 0;
+    int limit;
 
-    for (int i = 0; i < tamanho; i++){
-       
-        soma += vetor[i];
-        if (soma >= barra){
-            cont++;
-            soma = 0;
-            if(tamanhoAux == capacidadeAux){
-                capacidadeAux += 1;
-                vetorAux = (int *) realloc(vetorAux, capacidadeAux * sizeof(int));
+    while (1) {
+        scanf("%d", &value);
+
+        if (value == 0) {
+            if (scanf("%d", &limit) == EOF) {
+                break;
             }
-            vetorAux[tamanhoAux] = i;
-            tamanhoAux++;
+
+            Node *current = subsequence;
+            Node *prev = NULL;
+            while (current != NULL) {
+                sum += current->value;
+                if (sum >= limit) {
+                    printAndFree(&current);
+                    subsequence = prev;
+                    break;
+                }
+                prev = current;
+                current = current->next;
+            }
+            sum = 0;
+        } else {
+            insert(&subsequence, value);
         }
     }
-    
-    for (int i = tamanhoAux - 1; i >= 0; i--){
-        printf("%d\n",vetor[vetorAux[i]]);
-    }
-   
-    free(vetor); 
-    free(vetorAux); 
+
     return 0;
 }
