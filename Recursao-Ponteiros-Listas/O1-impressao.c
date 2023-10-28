a468eec9146e8e247d8774c3b1656f47fc7be5a3
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-28 09:09:50 -0300

    O1 impressao

---
+++ Recursao-Ponteiros-Listas/O1-impressao.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}celula;
+
+void imprime(celula *le);
+void imprime_recursivo(celula *le);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+void imprime(celula *le){
+
+}
+
+void imprime_recursivo(celula *le){
+    
+}
