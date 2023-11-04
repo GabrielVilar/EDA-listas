2c854c2ac8e9a41722d1bbf0f178567ab735397e
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-04 17:18:55 -0300

    R1 - remoção (correto)

--- Recursao-Ponteiros-Listas/R1-remocao.c
+++ Recursao-Ponteiros-Listas/R1-remocao.c
@@ -1,28 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct celula{
+typedef struct celula {
     int dado;
     struct celula *prox;
-}celula;
+} celula;
 
-int remove_depois(celula *p);
-void remove_elemento(celula *le, int x);
-void remove_todos_elementos(celula *le, int x);
+int remove_depois(celula *p) {
+    if (p == NULL || p->prox == NULL) {
+        return 0;  // Não é possível remover o elemento seguinte
+    }
 
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
+    celula *temp = p->prox;
+    p->prox = temp->prox;
+    free(temp);
+    return 1;  // Remoção bem-sucedida
 }
-// Remove depois
-int remove_depois(celula *p){
 
-}
-// Remove elementos
-void remove_elemento(celula *le, int x){
+void remove_elemento(celula *le, int x) {
+    celula *atual = le;
 
+    while (atual->prox != NULL) {
+        if (atual->prox->dado == x) {
+            celula *temp = atual->prox;
+            atual->prox = temp->prox;
+            free(temp);
+            return;  // Remoção da primeira ocorrência bem-sucedida
+        }
+        atual = atual->prox;
+    }
 }
-// Remove todos os elementos
-void remove_todos_elementos(celula *le, int x){
-    
+
+void remove_todos_elementos(celula *le, int x) {
+    celula *atual = le;
+
+    while (atual->prox != NULL) {
+        if (atual->prox->dado == x) {
+            celula *temp = atual->prox;
+            atual->prox = temp->prox;
+            free(temp);
+        } else {
+            atual = atual->prox;
+        }
+    }
 }
+
+void imprime(celula *le) {
+    celula *atual = le->prox;
+    while (atual != NULL) {
+        printf("%d -> ", atual->dado);
+        atual = atual->prox;
+    }
+    printf("NULL\n");
+}
+
