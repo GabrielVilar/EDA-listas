badc1a50092c79ae557de38215ae8c4619cb0e9b
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-04 16:43:31 -0300

    P1 - inserção

--- Recursao-Ponteiros-Listas/P1-insercao.c
+++ Recursao-Ponteiros-Listas/P1-insercao.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct celula{
+typedef struct celula {
     int dado;
     struct celula *prox;
-}celula;
+} celula;
 
-void insere_inicio(celula *le, int x);
-void insere_antes(celula *le, int x, int y);
-
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
+void insere_inicio(celula *le, int x) {
+    celula *novo_elemento = (celula *)malloc(sizeof(celula));
+    novo_elemento->dado = x;
+    novo_elemento->prox = le->prox;
+    le->prox = novo_elemento;
 }
 
-void insere_inicio(celula *le, int x){
+void insere_antes(celula *le, int x, int y) {
+    celula *atual = le;
+    while (atual->prox != NULL) {
+        if (atual->prox->dado == y) {
+            celula *novo_elemento = (celula *)malloc(sizeof(celula));
+            novo_elemento->dado = x;
+            novo_elemento->prox = atual->prox;
+            atual->prox = novo_elemento;
+            return;
+        }
+        atual = atual->prox;
+    }
 
+    
+    celula *novo_elemento = (celula *)malloc(sizeof(celula));
+    novo_elemento->dado = x;
+    novo_elemento->prox = NULL;
+    atual->prox = novo_elemento;
 }
 
-void insere_antes(celula *le, int x, int y){
-    
+void imprime(celula *le) {
+    celula *atual = le->prox;
+    while (atual != NULL) {
+        printf("%d -> ", atual->dado);
+        atual = atual->prox;
+    }
+    printf("NULL\n");
 }
