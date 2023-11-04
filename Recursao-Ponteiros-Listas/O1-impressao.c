863f2b690d2f606039c6fb4b5a56ce1e855aeeeb
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-04 16:16:26 -0300

    O1 - impressao

--- Recursao-Ponteiros-Listas/O1-impressao.c
+++ Recursao-Ponteiros-Listas/O1-impressao.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-typedef struct celula{
+typedef struct celula {
     int dado;
     struct celula *prox;
-}celula;
+} celula;
 
-void imprime(celula *le);
-void imprime_recursivo(celula *le);
-
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
+void imprime(celula *le) {
+    celula *atual = le->prox;
+    while (atual != NULL) {
+        printf("%d -> ", atual->dado);
+        atual = atual->prox;
+    }
+    printf("NULL\n");
 }
 
-void imprime(celula *le){
-
-}
-
-void imprime_recursivo(celula *le){
-    
+void imprime_rec(celula *le) {
+    if (le->prox == NULL) {
+        printf("NULL\n");
+        return;
+    }
+    printf("%d -> ", le->prox->dado);
+    imprime_rec(le->prox);
 }
