ab8d60e52cc6c706f8b7ee443f53a29f65092490
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-06 14:17:45 -0300

    L1 ultrapassa minimamente

--- Recursao-Ponteiros-Listas/L1-ultrapassa-minimamente.c
+++ Recursao-Ponteiros-Listas/L1-ultrapassa-minimamente.c
@@ -248,3 +248,49 @@ int main() {
     return 0;
 }
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+// Versão correta!
+void ultrapassa_minimamente(int *number, int limit, int index, int size)
+{
+    if ((index) < size)
+    {
+        int sum = 0;
+
+        while (sum <= limit && index < size)
+        {
+                sum += number[index];
+                (index)++;
+        }
+
+        ultrapassa_minimamente(number, limit, index, size);
+
+        if ((index) < size)
+        {
+            printf("%d\n", number[index - 1]);
+    }
+}
+}
+
+int main(){
+    
+    int *number = NULL;
+    int numero = 1;
+    int capacity = 0;
+    int limit, index = 0;
+
+    while (numero != 0)
+    {
+        capacity++;
+        scanf("%d", &numero);
+        number = realloc(number, capacity * sizeof(int));
+        number[capacity - 1] = numero;
+    }
+
+    scanf("%d", &limit);
+
+    ultrapassa_minimamente(number, limit, index, capacity);
+
+    return 0;
+}
