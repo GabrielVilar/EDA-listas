475f1188b38da81873fa106f38f54914f23b434b
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2024-04-02 19:40:49 -0300

    Changes

---
+++ EDA-2/L1 Formativa-1-Revisao/B-Soma-de-dois-numeros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    
+    int num1 = 0;
+    int num2 = 0;
+    int soma = 0;
+
+    scanf("%d", & num1);
+    scanf("%d", & num2);
+
+    soma = num1 + num2;
+
+    printf("%d\n", soma);
+}
